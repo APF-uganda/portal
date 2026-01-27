076bde1ff9db829fe3853a68972a22ac2012fe40
kikomeko bashir musa <kikomekobashir29@gmail.com>
2026-01-27 17:12:26 +0300

    dasboard structure set

---
+++ src/components/common/memberSideNav.tsx
@@ -0,0 +1,8 @@
+function MemberSideNav() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+export default MemberSideNav
---
+++ src/pages/admin/adminDashboard.tsx
@@ -0,0 +1,12 @@
+
+import adminSideNav from '../components/common/adminSideNav'
+
+function adminDashboard() {
+  return (
+    <div>
+      <adminSideNav />
+     
+    </div>
+  )
+}
+export default adminDashboard
